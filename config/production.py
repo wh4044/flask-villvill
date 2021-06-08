81513e98b4b91ef11fa02cb98da855f9a9bddd3e
진완혁 <axe_knife@naver.com>
2021-06-08 18:22:45 +0900

    'config'

--- config/production.py
+++ config/production.py
@@ -1,8 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'villvill.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = 'dev'
+SECRET_KEY = b'\xcfP\xd8\xe8\xe36]\x7f}F\x8f\xda\xabc\x95\xe3'
