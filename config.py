29dd754b793f15bdf66c35ecc5f06e721e5c83b5
진완혁 <axe_knife@naver.com>
2021-06-10 16:36:36 +0900

    'start'

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-from config.default import *
+import os
+
+BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'villvill.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
