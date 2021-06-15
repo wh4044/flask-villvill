05ac31541a4eb91e1f66db94e7b7f324c27bb3e2
진완혁 <axe_knife@naver.com>
2021-06-15 16:05:51 +0900

    'DB'

--- config/production.py
+++ config/production.py
@@ -2,7 +2,11 @@ from logging.config import dictConfig
 
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'villvill.db'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user='dbmasteruser',
+    pw='-^gQ!rTSQnjSpiw00gpY<cSR,a.(dIRR',
+    url='ls-74b32ab5c29f834d698aad3735e1b597d4622a74.culcdf0budht.ap-northeast-2.rds.amazonaws.com',
+    db='flask_villvill')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'n\x86=+N\xff\x7f\xf8\xfd\xb5\xfdN\xfc\xe6?\x13'
 
