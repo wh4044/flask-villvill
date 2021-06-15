c678514790d7a8e4d652f4f38e56da8646702caa
진완혁 <axe_knife@naver.com>
2021-06-15 17:33:31 +0900

    'pw'

--- villvill/models.py
+++ villvill/models.py
@@ -67,7 +67,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     nickname = db.Column(db.String(15), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
     phone = db.Column(db.String(64), unique=True, nullable=False)
 
